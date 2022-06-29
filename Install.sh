9b15e1e441327323ba1ef0ef90bb9fc1edbbdaeb
Mahdiyar Faqihi <82592464+Mahdiyar-Faqihi@users.noreply.github.com>
2022-06-29 16:02:47 +0430

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,20 +3,20 @@
 case $USER in 
     root)
     	echo "$(mkdir /usr/bin/LiriMain)"
-  		echo "$(cp ~/assistant.sh /usr/bin/LiriMain)"
-  		echo -e "export Liri=/usr/bin/LiriMain/assistant.sh" >> /root/.bashrc
-  		echo -e "alias liri="$Liri"" >> /root/.bashrc
-  		echo -e "$(source /root/.bashrc)"
-  		echo -e "$(source /root/.bashrc)"
-  		;;
-  	*)
-			echo "$(mkdir /home/$USER/LiriMain)"
-  			echo "$(cp ~/assistant.sh /home/$USER/LiriMain)"
-  			echo -e "export Liri=/home/$USER/LiriMain/assistant.sh" >> /home/$USER/.bashrc
-  			echo -e "alias liri="$Liri"" >> /home/$USER/.bashrc
-  			echo -e "$(source /home/$USER/.bashrc)"
-		    echo -e "$(source /home/$USER/.bashrc)"
-		    ;;
+  	echo "$(cp ~/assistant.sh /usr/bin/LiriMain)"
+  	echo -e "export Liri=/usr/bin/LiriMain/assistant.sh" >> /root/.bashrc
+	echo -e "alias liri="$Liri"" >> /root/.bashrc
+  	echo -e "$(source /root/.bashrc)"
+  	echo -e "$(source /root/.bashrc)"
+  	;;
+    *)
+	echo "$(mkdir /home/$USER/LiriMain)"
+  	echo "$(cp ~/assistant.sh /home/$USER/LiriMain)"
+  	echo -e "export Liri=/home/$USER/LiriMain/assistant.sh" >> /home/$USER/.bashrc
+  	echo -e "alias liri="$Liri"" >> /home/$USER/.bashrc
+  	echo -e "$(source /home/$USER/.bashrc)"
+        echo -e "$(source /home/$USER/.bashrc)"
+	;;
 esac
   
 
