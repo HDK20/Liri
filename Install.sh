bedf47ba0a701b442d4be20e5991221415de9383
Mahdiyar Faqihi <82592464+Mahdiyar-Faqihi@users.noreply.github.com>
2022-06-29 15:47:14 +0430

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $USER == root ]]: then
+  echo "$(mkdir /usr/bin/LiriMain)"
+  echo "$(cp ~/assistant.sh /usr/bin/LiriMain)"
+  echo -e "export Liri=/usr/bin/LiriMain/assistant.sh" >> /root/.bashrc
+  echo -e "alias liri="$Liri"" >> /root/.bashrc
+  echo -e "$(source /root/.bashrc)"
+  echo -e "$(source /root/.bashrc)"
+else
+  echo "$(mkdir /home/$USER/LiriMain)"
+  echo "$(cp ~/assistant.sh /home/$USER/LiriMain)"
+  echo -e "export Liri=/home/$USER/LiriMain/assistant.sh" >> /home/$USER/.bashrc
+  echo -e "alias liri="$Liri"" >> /home/$USER/.bashrc
+  echo -e "$(source /home/$USER/.bashrc)"
+  echo -e "$(source /home/$USER/.bashrc)"
+fi  
+  
+  
+  
+  
+
+
+
