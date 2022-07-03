f89f2d1a4aeaac5ccf9b872eaf374210792de74e
Mahdiyar Faqihi <82592464+Mahdiyar-Faqihi@users.noreply.github.com>
2022-07-03 21:53:17 +0430

    Update assistant.sh

--- assistant.sh
+++ assistant.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 #this is a small assistant programm
 
@@ -9,8 +9,12 @@ yellow='\033[0;33m'
 blue='\033[0;34m'
 magenta='\033[0;35m'
 cyan='\033[0;36m'
+gray='\033[0;90m'
+white='\033[1;37m'
 # Clear the color after that
 clear='\033[0m'
+#----------------------------------
+
 
 
 
@@ -18,17 +22,18 @@ echo "
 © Created by Mahdiyar
 "
 
-echo "Hi $USER, I'm Liri" | boxes -d face
+echo -e "Hi ${white}$USER${clear}, I'm Liri" | boxes -d face
 
 while [ true ]
 do
 
 # part that input your command and process it
 	echo -ne "
-How can I help you (Without space😉)
+How can I help you 😉
 
-$USER :${magenta} "
+$USER :${blue} "
 	read Want
+	
 
 	echo -e "${clear}"
 #end input
@@ -44,34 +49,39 @@ $USER :${magenta} "
 			;;
 
 		exit | q | Q)
-			echo -e "${green}\t\t\t --\ Good by! /-- ${clear}"
+			echo -e "${green}\t\t\t\t --\ Good by! /-- ${clear}"
 			exit 0
 			;;
 
 		hello | hi)
 			echo "Hi, nice to meet you."
 			;;
-
-
+	
+		comm | command | com)
+			echo -n "Enter the Command : "
+			read -a comand 
+			echo "$($comand)"
+			;;
+		        	
 		#a part that open IDE
 		codeeditor | code | ide)
-        	echo -n "
-Which editor do you want to open?(enter IDE name without space/ n = no): "
+        	echo -en "
+Which editor do you want to open?(enter IDE name without space/ n = no): ${yellow}"
 
 			read IDE
-
+			echo -en "${clear}"
 			case $IDE in
 
 				vscode | visualstdio)
-					echo "$(code &)"
+					echo "$(code &> /dev/null &)"
 					;;
 				
 				sublim | sublimtext)
-					echo "$(subl &)"
+					echo "$(subl &> /dev/null &)"
 					;;
 				
 				codeblocks)	
-					echo "$(codeblock &)"
+					echo "$(codeblock &> /dev/null &)"
 					;;
 
 				vim | vi)
@@ -79,14 +89,14 @@ Which editor do you want to open?(enter IDE name without space/ n = no): "
  					;;
  				
 				emacs)
-					echo "You are still using emacs? ಠ‿ಠ " && sleep 1 && echo "$(emacs &)"
+					echo "You are still using emacs? ಠ‿ಠ " && sleep 1 && echo "$(emacs &> /dev/null &)"
 					;;
 				n)
 					echo "Get away from the front of my eyes."
 					;; 
 	
 				*)
-					echo "We dont like $IDE"
+					echo "We dont Support $IDE"
 					;;
 
 			esac
@@ -97,37 +107,39 @@ Which editor do you want to open?(enter IDE name without space/ n = no): "
 			#open a browser
 
 		browser | url | Browser | BROWSER)
-			echo -n "Do you want to open a Browser? (f = firefox, b = brave, c = chrome, n = no): "
+			echo -en "Do you want to open a Browser? (f = firefox, b = brave, c = chrome, n = no): ${yellow}"
 			read Browser
+			echo -en "${clear}"
 
 			if [[ $Browser == n ]]; then
 				Url=""
 			else
-				echo -n "If you want the open a site enter the url : "
+				echo -en "If you want the open a site enter the url : ${yellow}"
 				read Url
+				echo -en "${clear}"
 			fi
 
 			case $Browser in
 	
 			f | F)
-	  			echo "$(nohup firefox $Url &)"  	  
+	  			echo "$(firefox $Url &> /dev/null &)"  	  
 	  			;;
 
 			b | B)
-      			echo "$(nohup brave-browser -a $Url &)"
-      			;;
+	      			echo "$(brave-browser -a $Url &> /dev/null &)"
+      				;;
 
-    		c | C)
-		    	echo "$(nohup chrome $Url &)"
-      			;;
+    			c | C)
+			    	echo "$(chrome $Url &> /dev/null &)"
+      				;;
 
-    		n | N)
-      			echo "are you kidding me (◉_◉)"
-      			;;
-
-    		*)
-      			echo "are you kidding me (◉_◉)"
-      			;;
+    			n | N)
+      				echo -e "${cyan}are you kidding me (◉_◉)${clear}"
+      				;;
+	
+    			*)
+      				echo -e "${cyan}are you kidding me (◉_◉)${clear}"
+      				;;
 			esac
 			;;
 			#end browser part
@@ -138,33 +150,60 @@ Which editor do you want to open?(enter IDE name without space/ n = no): "
 			echo "On your own system or spotify?(o/s)"
 			read location
 			if [[ $location == o ]]; then
-    			echo "$(dolphin home/$USER/Music)"
+    				echo "$(dolphin /home/$USER/Music)"
 			else
-    			echo "$(firefox https://www.spotify.com &)"
+    				echo "$(firefox https://www.spotify.com &> /dev/null &)"
 			fi
 			;;
 
+		vpn | vpn\ on | vpnon)
+			echo "$(windscribe-cli connect best)"
+			;;
+
+		vpnoff | vpndown)
+			echo "$(windscribe-cli disconnect)"
+			;;
 
 		clear)
-			echo -e "${cyan}We are slovenly ¯\(©¿©) /¯ ${clear}"
+			echo "$(clear)"
 			;;
 
 		alphabet)
-			echo {A..Z}
+			echo -n {A..Z}
+			echo -e "\n"
 			echo {a..z}
 			;;
-		
-		love | iloveyou)
+
+		whoami)
+			echo "You are $USER"
+			;;
+
+		pwd)
+			echo -e "We are in ${yellow}$(pwd)${clear}"
+			;;
+
+		ls)
+			echo -e "${yellow}$(ls -la)${clear}"
+			;;	
+
+		love | iloveyou | i\ love\ you)
 			echo -e "Shut up! I have robotfriend ${red}┗(｀o ´)┓${clear}"
 			;;
+
+		i\ hate\ you)
+			echo -e "${red}me too${clear}"
+			;;
+
+		liri | Liri | LIRI)
+			echo "I'm here"
+			;;
+	
 		*)
 			echo -e "${red}...........Undefined...........${clear}"
-			
+				
 	esac
 
-	echo "
-
-	"	
+	echo -e "\n\n"	
 
 done
 
